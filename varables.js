b579d3befcd3cd99b51d7503819c625a354e7520
Abhishek <abhighadage2622002@gmail.com>
2024-12-02 18:21:18 +0530

    varable declaration code

---
+++ varables.js
@@ -0,0 +1,11 @@
+const accountId=12525;
+let accEmail="Abhi@gmail.com"
+var password=12522
+
+city="pune"
+accEmail="rohan@gmail.com"
+password=1552
+// accountId=1252--> this is not allowed with const keyword const can not allowd for redeclaration
+console.log(accountId);
+
+console.table([accountId,accEmail,password.city])
