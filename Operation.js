b2106087323c597094b9972ff99efcc3d4522a49
Abhishek <abhighadage2622002@gmail.com>
2024-12-03 12:45:34 +0530

    operation file in js

---
+++ Operation.js
@@ -0,0 +1,20 @@
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(3+4*5/2);//this statement dont write like that do code readable and structure format
+console.log((3+4)*2/3);//write code like that
+
+console.log(true);
+console.log(false);
+
+console.log(+true);
+console.log(+false);
+
+let i=100;
+i++//postfix
+console.log(i);//post increment
+
+++i//prefix
+console.log(i);//pre-increment
