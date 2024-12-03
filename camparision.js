9041be58c85f0d43d9ecd66c4d492bd0623f7ce8
Abhishek <abhighadage2622002@gmail.com>
2024-12-03 17:00:56 +0530

    null undefined triple equal examples code

---
+++ camparision.js
@@ -0,0 +1,25 @@
+// //this is a comparion operator in js
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2!=1);
+
+// console.log("=====================================");
+// //null is greater than zero
+console.log(null>0);
+console.log(null<0);
+console.log(null==0);
+console.log(null>=0);
+// console.log("=====================================");
+// //undefined is less than zero
+console.log(undefined>0);
+console.log(undefined<0);
+console.log(undefined==0);
+console.log(undefined>=0);
+
+console.log("=====================================");
+//=== this a strict check it check or compare the value and also compare and check it datatypes same or not
+
+console.log("2"=== 2);//here value is same but datatype is different so output get fasle
+
