7f34b677d25746c0af7c60caee47a36b783c0bb3
Abhishek <abhighadage2622002@gmail.com>
2024-12-03 10:04:07 +0530

    datatypes and last file changes

---
+++ dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"//this used for treat all js code as newer version older js when we use as newer js that time we use the use strict keyword 
+
+// alert("Abhishek")//here not allowed alert in node js alert is used in browser 
+
+
+// console.log(3+3) console.log("Abhi")This is not allowed 
+console.log(3+3); console.log("Abhi")//This is allowed with the semicolon but dont write it like that  organize in proper way
+
+
+//this is proper sturcture format
+console.log(3+3)
+console.log("Abhishek")
+
+//datatypes (primitive)
+let number=1262//number datatype
+let str="Abhishek"// String datatype
+let isPresent=false //boolean datatypes
+let val=null//null is standlone value or datatype
+let no//this a undefined datatype
+//symbol
+
+
+// datatype (Object types datatypes)
+
+
+console.log(typeof(number))
--- varables.js
+++ varables.js
@@ -3,9 +3,10 @@ let accEmail="Abhi@gmail.com"
 var password=12522
 
 city="pune"
+let name;
 accEmail="rohan@gmail.com"
 password=1552
 // accountId=1252--> this is not allowed with const keyword const can not allowd for redeclaration
 console.log(accountId);
 
-console.table([accountId,accEmail,password.city])
+console.table([accountId,accEmail,password.city,name])
