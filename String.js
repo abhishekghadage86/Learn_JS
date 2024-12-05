a0e676f4d664f3f9511cb0ae2cdb360e99447ab9
Abhishek <abhighadage2622002@gmail.com>
2024-12-05 13:07:08 +0530

    String Operation

---
+++ String.js
@@ -0,0 +1,24 @@
+const name="Abhishek"
+const repoCount=50
+//this is a string interpollation method used with the ----->${}
+console.log(`Hello my name is ${name} and repo counnt is ${repoCount}`)
+
+const str=new String('Abhishek-Sunil-Ghadage')
+console.log(str);//string print
+console.log(str[1]);//string inndex 1 char print
+console.log(str.length);//find of length string
+console.log(str.toUpperCase());//convert string into uppercase
+console.log(str);//print string
+console.log(str.charAt(2));//use the char using index using print char
+console.log(str.indexOf('h'))//find the index of char
+console.log(str.substring(1,5));//find the substring into string using the start and last index.it work with only positive number
+console.log(str.slice(-8,4));//it substring find type it work with postive and negative  
+const str1="    Abhishek "//de fine new string
+console.log(str1);
+console.log(str1.trim());//it used for remove the white spaces in string
+console.log(str.split('-'))//split string with help coma -
+
+const url="http//:Abhishek/demo%20/javascript"//define url string
+console.log(url.replace('20','-'));//replace the number in  - number or space not allowed in url
+console.log(url.replace('Ab','AB'))
+
