edecd910999084f45cfb4d309e03734ee329b4b4
Abhishek <abhighadage2622002@gmail.com>
2024-12-10 19:22:04 +0530

    arrow function in js

--- Arrow-function.js
+++ Arrow-function.js
@@ -16,6 +16,51 @@ user.welcomeMessage()
 console.log("===================================================");
 console.log(this);//it is print the empty object 
 
+console.log("++++++++++++++++++++++++++++++++++++++++||---Arrow Function---||+++++++++++++++++++++++++++++++++++++++++++")
+// function chai()
+// {
+//     let uname="Abhishek"
+//     console.log(this.uname)
+// }
+// function chai()
+// {
+//     let uname="Abhishek"
+//     console.log(this.uname)//op is undefined
+// }
+// chai()
+
+// let chai=function()
+// {
+//     const uname="Abhi"
+//     console.log(this.uname);//op is undefined
+    
+// }
+
+//this is arrow function 
+let chai= ()=>{
+    const uname="Abhi"
+    console.log(this.uname);//op is undefined
+    
+}
+chai();
+
+// const addTwo=(num1,num2)=>{
+// return num1+num2
+// }
+// console.log(addTwo(10,20));
+
+//this is another or code reduce tech  type of arrow function
+const addTwo=(num1,num2)=> num1+num2
+    console.log(addTwo(10,20));
 
 
-//++++++++++++++++++++++++++++++++++++++++Arrow Function+++++++++++++++++++++++++++++++++++++++++++
+ //=======================-----Object pass or return in arrow function---===========================
+ 
+ const firstObj=()=>{userName:"Abhishek";age:20};//op is undefined this is error of this program simple brace is reqired always
+ const firstObj1=()=>({userName:"Abhishek",age:20})
+
+
+ console.log(firstObj());
+ console.log(firstObj1());
+
+ 
