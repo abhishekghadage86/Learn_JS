b7e5a20a36ed49c5a9c151dbaac991f54d4e1a2d
Abhishek <abhighadage2622002@gmail.com>
2024-12-10 12:09:20 +0530

    call function scope

--- scope.js
+++ scope.js
@@ -5,6 +5,7 @@ function show()
     var c=300
 
 }
+show()
 // console.log(a);this has a block level and global scope
 // console.log(b);this has a block level and global scope
 console.log(c);//it has a local scope
