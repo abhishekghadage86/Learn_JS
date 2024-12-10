92440a95775f0ac806511a525cf92fabd575c4d0
Abhishek <abhighadage2622002@gmail.com>
2024-12-10 12:07:43 +0530

    scope of var const let

---
+++ scope.js
@@ -0,0 +1,10 @@
+function show()
+{
+    let a=100;
+    const b=200
+    var c=300
+
+}
+// console.log(a);this has a block level and global scope
+// console.log(b);this has a block level and global scope
+console.log(c);//it has a local scope
