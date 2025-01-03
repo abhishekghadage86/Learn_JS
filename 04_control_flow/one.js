3fea51f511cf9ca1d2352374619b61c76692134c
Abhishek <abhighadage2622002@gmail.com>
2025-01-03 17:56:54 +0530

    if and else if programs

---
+++ 04_control_flow/one.js
@@ -0,0 +1,29 @@
+//if 
+let num=120;
+if(num>100)
+{
+console.log("number is greater");
+}
+else
+{
+    console.log("number is not greater");
+    
+}
+
+const score=200;
+if(score>100)
+{
+ const power="fly"
+console.log(`The power is =${power}`);
+
+}
+// console.log(`The power is =${power}`); here power is not acessible for we get error this statement is not define in power scope variable
+if(true) console.log("Welcome");//this is short notation 
+if(true && true && true)//AND Operator
+    {
+    console.log("Login");
+    }
+    if(true || true || true)//OR operator
+        {
+        console.log("Login");
+        }
