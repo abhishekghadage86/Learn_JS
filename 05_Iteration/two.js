cac80745d32853a08280bf7b206ddacf0ba2b5dd
Abhishek <abhighadage2622002@gmail.com>
2025-01-04 18:05:42 +0530

    while and do while loop

---
+++ 05_Iteration/two.js
@@ -0,0 +1,23 @@
+//while
+let num=10;
+while (num!=0) {
+    console.log(num);
+    num--
+}
+let myArray=[10,20,30,40]
+let arr=0;
+while (arr<myArray.length) {
+    console.log(`Value is ${myArray[arr]}`);
+    arr++;
+
+}
+// output--->
+// Value is 10
+// Value is 20
+// Value is 30
+// Value is 40
+num=0
+do {
+    console.log("Abhi");
+    num++
+} while (num!=10);
